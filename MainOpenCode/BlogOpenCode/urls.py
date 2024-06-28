47c1b6924cff43a6e063e2a45d4346fbfe21693c
stefkucherov <stepan.kucherov@mail.ru>
2024-06-28 18:36:34 +0500

    The new interface also adds an image publishing feature.

--- MainOpenCode/BlogOpenCode/urls.py
+++ MainOpenCode/BlogOpenCode/urls.py
@@ -3,7 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
-urlpatterns = [path('', views.PostViev.as_view()),
-               path('', views.blog_list, name='blog_list'),
-               path('create/', views.create_post, name='create_post')] + static(settings.STATIC_URL,
-                                                                                document_root=settings.STATIC_ROOT)
+urlpatterns = [
+                  path('', views.PostViev.as_view()),
+                  path('', views.blog_list, name='blog_list'),
+                  path('create/', views.create_post, name='create_post')
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
