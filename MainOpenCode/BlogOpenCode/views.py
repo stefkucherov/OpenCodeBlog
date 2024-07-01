a0fa5bbfeda642a461f056e4775c96f3c53da16e
stefkucherov <stepan.kucherov@mail.ru>
2024-07-01 21:29:22 +0500

    The new interface also adds registration and authentication.

--- MainOpenCode/BlogOpenCode/views.py
+++ MainOpenCode/BlogOpenCode/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import View
+from django.views.generic import ListView
 from BlogOpenCode.models import Post
 from BlogOpenCode.forms import PostForm
-# Create your views here.
-
-# Вывод записей
-from django.views.generic import ListView
 from .models import Post
-
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
-from .models import Post
+from .forms import UserRegisterForm
 from .forms import PostForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
+
+# MainOpenCode/views.py
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('blog_list')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('blog_list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 
 class PostListView(ListView):
--- MainOpenCode/MainOpenCode/urls.py
+++ MainOpenCode/MainOpenCode/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from BlogOpenCode.views import register, login_view, logout_view
+
 
 urlpatterns = ([
                   path('admin/', admin.site.urls),
-                  path('', include('BlogOpenCode.urls'))
+                  path('', include('BlogOpenCode.urls')),
+                  path('register/', register, name='register'),
+                  path('login/', login_view, name='login'),
+                  path('logout/', logout_view, name='logout'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
                 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
-
